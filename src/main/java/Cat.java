7abcf50e537010ecb7277211c430f4bc3d8160e9
Zagiruf <Zagiruf@gmail.com>
2024-03-10 04:00:18 +0500

    commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+import java.util.Objects;
+
+public class Cat {
+    String cat;
+    public String getCat() {
+        return cat;
+    }
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+}
